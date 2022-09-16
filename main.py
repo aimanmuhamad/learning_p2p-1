a0e9d7bdba84c1146194afc80c3f45ad36926e5c
Aiman Muhamad <80809455+aimanmuhamad@users.noreply.github.com>
2022-09-16 13:13:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from math_function import add, root
+
+
+def main():
+    operator = input("masukkan operator :")
+    if operator == "+":
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
+        result = add(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    
+    if operator == "root" :
+        data_1 = int(input("masukkan input 1 :"))
+        result = root(data_1)
+        
+        print(result)
+    
+if __name__ == "__main__":
+    print("Hello Main !")
+    main()
