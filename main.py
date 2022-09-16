82a62e1110c1e29548031cbc857dea803726e059
Fiqhi Setya Wananda <fiqhisetya5@gmail.com>
2022-09-16 13:55:44 +0700

    First commit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from math_function import add
+from math_function import add, root
 
 
 def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
-
     if operator == "+":
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
         result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
-
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    
+    if operator == "root" :
+        data_1 = int(input("masukkan input 1 :"))
+        result = root(data_1)
+        
+        print(result)
+    
 if __name__ == "__main__":
     print("Hello Main !")
     main()
