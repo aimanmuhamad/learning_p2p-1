20b0327e0791e3af33e213ad235e6d57df471d87
Fanny Audri <fannyausri09@gmail.com>
2022-09-16 17:37:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from math_function import add
+from math_function import add, exponential
 
 
 def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
-
     if operator == "+":
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
         result = add(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    if operator == "exponential" :
+        data_1 = int(input("masukkan input 1:"))
+        result = exponential(data_1)
 
+        print(result)
 
 if __name__ == "__main__":
     print("Hello Main !")
--- math_function.py
+++ math_function.py
@@ -1,3 +1,9 @@
+import math
 
 def add(a, b):
     return a + b
+
+def exponential(a):
+    return math.exp(a)
+
+
