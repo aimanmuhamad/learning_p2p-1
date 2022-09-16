3805fbb48f2b469c7a51b6c6adcd0907f388483d
aimanm135 <aimanm135@gmail.com>
2022-09-16 21:27:08 +0700

    add feature max_number

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from math_function import add
+from math_function import add, max_number
 
 
 def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
-
     if operator == "+":
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
         result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
-
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    
+    if operator == "maximum number" :
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
+        result = max_number(data_1, data_2)
+        
+        print(result)
+    
 if __name__ == "__main__":
     print("Hello Main !")
     main()
--- math_function.py
+++ math_function.py
@@ -1,3 +1,5 @@
-
 def add(a, b):
     return a + b
+
+def max_number(a,b):
+    return max(a,b)
