4ce10cd02bab038734c13194be691a8e88afde26
Fanny Audri <fannyausri09@gmail.com>
2022-09-16 17:37:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from math_function import add
 
 
 def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
-
     if operator == "+":
+        data_1 = int(input("masukkan input 1 :"))
+        data_2 = int(input("masukkan input 2 :"))
         result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    
+    if operator == "root" :
+        data_1 = int(input("masukkan input 1 :"))
+        result = root(data_1)
+        
+        print(result)
 
 
 if __name__ == "__main__":
