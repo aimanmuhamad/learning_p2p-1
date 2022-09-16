db066a53fea19f090c37fbd0092c7c3148a8ddaa
hpynafrh <113113752+hpynafrh@users.noreply.github.com>
2022-09-16 13:22:05 +0700

    add new function root

--- math_function.py
+++ math_function.py
@@ -1,3 +1,12 @@
+import math
 
 def add(a, b):
     return a + b
+
+def root(a):
+    if a > 0 :
+        return math.sqrt(a)
+    elif a < 0 :
+        str1 = f"Can't find root for {a}"
+        return str1
+    
